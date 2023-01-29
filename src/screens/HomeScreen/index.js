e92ab49932d37f91be1fcd7dd0e1f075c5a42667
santosh-at-smarthub <santosh.parashetti@smarthub.ai>
2023-01-29 22:15:17 +0530

    Fix gradle version

--- src/screens/HomeScreen/index.js
+++ src/screens/HomeScreen/index.js
@@ -1,21 +1,28 @@
-import React, {Component} from 'react';
-import {Text, SafeAreaView} from 'react-native';
-import {connect} from 'react-redux';
+import React, { Component } from "react";
+import { Text, SafeAreaView } from "react-native";
+import { connect } from "react-redux";
 
 class Home extends Component {
-  componentDidMount () {
+  componentDidMount() {
     // console.log("test device", this.props);
   }
-  render () { 
-    return ( 
+  render() {
+    return (
       <SafeAreaView>
-        <Text> This is Home Screen </Text>
+        <Text
+          adjustsFontSizeToFit={true}
+          numberOfLines={2}
+          style={{ textAlign: "center", fontSize: 50 }}
+        >
+          {" "}
+          RAJIV BHAI, NAMASKAR!!!
+        </Text>
       </SafeAreaView>
     );
   }
 }
 
 const mapStateToProps = (state) => ({
-  ...state.appConfig
+  ...state.appConfig,
 });
 export default connect(mapStateToProps)(Home);
